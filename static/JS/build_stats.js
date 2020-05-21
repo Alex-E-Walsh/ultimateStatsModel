2fb583bcef19018686bb07443c9b213411193bf6
Alex-E-Walsh <aewalsh16@gmail.com>
2020-05-21 14:12:57 -0600

    added fancy table

--- static/JS/build_stats.js
+++ static/JS/build_stats.js
@@ -1,52 +1,48 @@
-$(document).ready(function () {
-
 
   function cleanStat(stat){
     stat = stat.split(",")
     stat.pop()
-    stat[0] = stat[0].slice(17)
-    stat[18] = stat[18].slice(3,8)
+    stat[0] = stat[0].slice(16)
+    stat[18] = stat[18].slice(1,6)
     stat = stat.map(Number)
     return stat
   }
+  var homevals = cleanStat(document.getElementById('stathome').innerText)
+  var awayvals = cleanStat(document.getElementById('stataway').innerText)
 
-  var homevals = cleanStat($('#stathome').text())
-  var awayvals = cleanStat($('#stataway').text())
+  var hometeam = document.getElementById('result').innerText.split(" ")[0].split('\n')[1]
+  var awayteam = document.getElementById('result').innerText.split(" ")[3]
 
+    // document.getElementById('cols').innerHTML = (
+    //   `<th>Team</th>
+    //   <th>G</th>
+    //   <th>W</th>
+    //   <th>L</th>
+    //   <th>T</th>
+    //   <th>PTS</th>
+    //   <th>PPG</th>
+    //   <th>PA</th>
+    //   <th>PAPG</th>
+    //   <th>OEFF</th>
+    //   <th>DEFF</th>
+    //   <th>+/-</th>
+    //   <th>+/- PG</th>
+    //   <th>BLK</th>
+    //   <th>BPG</th>
+    //   <th>CMP</th>
+    //   <th>CPG</th>
+    //   <th>CMP%</th>
+    //   <th>TO</th>
+    //   <th>TPG</th>`)
 
-  function buildTableRow(arr){
-    rowHTML = []
-    for(let i =0;i<arr.length;i++){
-      var td = "".concat("<td>",arr[i],"<td>")
-      rowHTML.push(td)
-    }
-    return rowHTML
-  }
+  function makeTable(){
+    document.getElementById('awayteam').innerHTML
+    document.getElementById('homestat').insertCell(0).innerHTML = hometeam
+    document.getElementById('awaystat').insertCell(0).innerHTML = awayteam
+    for(let i =0;i<homevals.length;i++){
+        document.getElementById('homestat').insertCell(i+1).innerHTML = homevals[i]
+        document.getElementById('awaystat').insertCell(i+1).innerHTML = awayvals[i]
+      }
+    };
 
-
-    $('#cols').html(
-      `<th>G</th>
-      <th>W</th>
-      <th>L</th>
-      <th>T</th>
-      <th>PTS</th>
-      <th>PPG</th>
-      <th>PA</th>
-      <th>PAPG</th>
-      <th>OEFF</th>
-      <th>DEFF</th>
-      <th>+/-</th>
-      <th>+/- PG</th>
-      <th>BLK</th>
-      <th>BPG</th>
-      <th>CMP</th>
-      <th>CPG</th>
-      <th>CMP%</th>
-      <th>TO</th>
-      <th>TPG</th>`)
-
-    console.log(buildTableRow(homevals))
-    $('#homestat').html(buildTableRow(homevals))
-
-    $('#awaystat').html(buildTableRow(awayvals))
-    })
+  makeTable();
